86229234ad4242c4378b4eeea3ce83433d118f1b
Dan <78473524+dankerow@users.noreply.github.com>
2023-03-21 17:52:35 -0700

    feat: Added generatePassword & toArray method

---
+++ src/utils/generatePassword.ts
@@ -0,0 +1,14 @@
+import crypto from 'crypto'
+
+export const generatePassword = (length) => {
+  const allowedChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]\:;?><,./-='
+  let password = ''
+  const randomBytes = crypto.randomBytes(length)
+
+  for (let i = 0; i < randomBytes.length; i++) {
+    const byte = randomBytes[i]
+    password += allowedChars[byte % allowedChars.length]
+  }
+
+  return password;
+}
