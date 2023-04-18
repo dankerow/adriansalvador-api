c57fbac7740bc0720fee5134f6ac251743082818
Dan <78473524+dankerow@users.noreply.github.com>
2023-04-18 09:51:26 -0700

    feat: renew

--- src/routes/images.ts
+++ src/routes/images.ts
@@ -1,34 +1,27 @@
 import { Route } from '../structures'
-import { join } from 'path'
+import { filesize } from 'filesize'
 
 export default class Images extends Route {
   constructor() {
     super({
       position: 2,
-      path: '/images'
+      path: '/images',
+      middlewares: ['auth']
     })
   }
 
   routes(app, options, done) {
-    const defaultFormat = 'webp'
-    const allowedFormats = ['png', 'jpeg', 'webp']
+    app.get('/:id', async (req, res) => {
+      const includeAlbum = req.query.includeAlbum ?? false
+      const file = await app.database.getFileById(req.params.id, includeAlbum)
 
-    app.post('/:id', async (req, res) => {
-      const name = req.params.name
-      const width = req.query.width ? parseInt(req.query.width) : null
-      const height = req.query.height ? parseInt(req.query.height) : null
-      const fit = req.query.fit ?? null
-      const format = allowedFormats.includes(req.query.format) ? req.query.format : defaultFormat
+      if (!file) return res.code(404).send({ error: { status: 404, message: 'File not found.' } })
 
-      const image = await app.database.findFileByName(name)
-      if (!image) return res.code(404).send({ error: { status: 404, message: 'Image not found.' } })
-
-      const album = await app.database.getAlbumById(image.albumId)
-
-      const filePath = join('src', 'static', 'gallery', album.name, name)
-
-      res.status(204)
-    });
+      return {
+        ...file,
+        size: filesize(file.size)
+      }
+    })
 
     done()
   }
