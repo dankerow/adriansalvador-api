b1f500a69c94eed5cc859dfd853b124131dd4397
Dan <78473524+dankerow@users.noreply.github.com>
2023-04-18 09:44:48 -0700

    feat: change WEBSITE_SECRET to AUTH_SECRET (+ Lint)

--- src/routes/authentication.ts
+++ src/routes/authentication.ts
@@ -3,54 +3,54 @@ import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
 export default class Authentication extends Route {
-	constructor() {
-		super({
-			position: 2,
-			path: '/authentication'
-		})
-	}
+  constructor() {
+    super({
+      position: 2,
+      path: '/authentication'
+    })
+  }
 
-	async routes(app, opts, done) {
-		const createToken = (user) => {
-			return jwt.sign({
-				iss: 'adriansalvador',
-				sub: user.id
-			}, process.env.WEBSITE_SECRET, { expiresIn: '3h' })
-		}
+  async routes(app, opts, done) {
+    const createToken = (user) => {
+      return jwt.sign({
+        iss: 'adriansalvador',
+        sub: user.id
+      }, process.env.AUTH_SECRET, { expiresIn: '3h' })
+    }
 
-		app.post('/login', async (req, res) => {
-			const email = req.body.email
-			const password = req.body.password
+    app.post('/login', async (req, res) => {
+      const email = req.body.email
+      const password = req.body.password
 
-			const user = await app.database.getUserByEmail(email)
-			if (!user) {
-				return res.code(403).send({ error: { statusCode: 403, message: 'Invalid credentials.' } })
-			}
+      const user = await app.database.getUserByEmail(email)
+      if (!user) {
+        return res.code(403).send({ error: { statusCode: 403, message: 'Invalid credentials.' } })
+      }
 
-			const userCredentials = await app.database.getUserCredentials(user.id)
+      const userCredentials = await app.database.getUserCredentials(user.id)
 
-			const passwordVerification = await bcrypt.compare(password, userCredentials.password)
-			if (passwordVerification) {
-				const token = createToken({ id: user.id })
+      const passwordVerification = await bcrypt.compare(password, userCredentials.password)
+      if (passwordVerification) {
+        const token = createToken({ id: user.id })
 
-				/* await app.database.updateUser(user.id, {
-					sessionToken: token
-				}) */
+        /* await app.database.updateUser(user.id, {
+          sessionToken: token
+        }) */
 
-				return { token, user }
-			} else {
-				return res.code(403).send({ error: { statusCode: 403, message: 'Invalid credentials.' } })
-			}
-		})
+        return { token, user }
+      } else {
+        return res.code(403).send({ error: { statusCode: 403, message: 'Invalid credentials.' } })
+      }
+    })
 
-		const authMiddleware = await import('../middlewares/auth.js')
+    const authMiddleware = await import('../middlewares/auth.js')
 
-		app.get('/verify', {
-			preHandler: [authMiddleware.default]
-		}, (req, res) => {
-			return req.user
-		})
+    app.get('/verify', {
+      preHandler: [authMiddleware.default]
+    }, (req) => {
+      return req.user
+    })
 
-		done()
-	}
+    done()
+  }
 }
