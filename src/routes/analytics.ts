ea9b25a5317cd20f13b44738fdf02e876c32a228
Dan <78473524+dankerow@users.noreply.github.com>
2023-04-18 09:43:54 -0700

    feat: added image count (+ Lint)

--- src/routes/analytics.ts
+++ src/routes/analytics.ts
@@ -5,183 +5,189 @@ import { BetaAnalyticsDataClient } from '@google-analytics/data'
 import key from '../keys/adrian-salvador-website-9e67e3e8a223.json' assert { type: 'json' }
 
 export default class Analytics extends Route {
-	constructor() {
-		super({
-			position: 2,
-			path: '/analytics',
-			middlewares: ['auth']
-		});
-	}
+  constructor() {
+    super({
+      position: 2,
+      path: '/analytics',
+      middlewares: ['auth']
+    })
+  }
 
-	routes(app, options, done) {
-		app.get('/', async (req, res) => {
-			const analyticsData = new BetaAnalyticsDataClient({
-				credentials: {
-					client_email: key.client_email,
-					private_key: key.private_key
-				}
-			})
+  routes(app, options, done) {
+    app.get('/', async () => {
+      const analyticsData = new BetaAnalyticsDataClient({
+        credentials: {
+          client_email: key.client_email,
+          private_key: key.private_key
+        }
+      })
 
-			const reports = {
-				basic: () => {
-					return {
-						property: 'properties/325424669',
-						dateRanges: [
-							{
-								startDate: '7daysAgo',
-								endDate: 'today'
-							}
-						],
-						metrics: [
-							{
-								name: 'screenPageViews'
-							},
-							{
-								name: 'totalUsers'
-							},
-							{
-								name: 'newUsers'
-							},
-							{
-								name: 'engagementRate'
-							}
-						]
-					}
-				},
-				popular: () => {
-					return {
-						property: 'properties/325424669',
-						dateRanges: [
-							{
-								startDate: '30daysAgo',
-								endDate: 'today'
-							}
-						],
-						dimensions: [
-							{
-								name: 'pagePath'
-							},
-							{
-								name: 'pageTitle'
-							}
-						],
-						metrics: [
-							{
-								name: 'screenPageViews'
-							}
-						],
-						orderBys: [
-							{
-								metric: {
-									metricName: 'screenPageViews'
-								},
-								desc: true
-							}
-						]
-					}
-				},
-				trending: () => {
-					return {
-						property: 'properties/325424669',
-						dateRanges: [
-							{
-								startDate: '1daysAgo',
-								endDate: 'today'
-							}
-						],
-						dimensions: [
-							{
-								name: 'pagePath'
-							},
-							{
-								name: 'pageTitle'
-							}
-						],
-						metrics: [
-							{
-								name: 'screenPageViews'
-							}
-						],
-						orderBys: [
-							{
-								metric: {
-									metricName: 'screenPageViews'
-								},
-								desc: true
-							}
-						]
-					}
-				}
-			}
+      const reports = {
+        basic: () => {
+          return {
+            property: 'properties/325424669',
+            dateRanges: [
+              {
+                startDate: '7daysAgo',
+                endDate: 'today'
+              }
+            ],
+            metrics: [
+              {
+                name: 'screenPageViews'
+              },
+              {
+                name: 'totalUsers'
+              },
+              {
+                name: 'newUsers'
+              },
+              {
+                name: 'engagementRate'
+              }
+            ]
+          }
+        },
+        popular: () => {
+          return {
+            property: 'properties/325424669',
+            dateRanges: [
+              {
+                startDate: '30daysAgo',
+                endDate: 'today'
+              }
+            ],
+            dimensions: [
+              {
+                name: 'pagePath'
+              },
+              {
+                name: 'pageTitle'
+              }
+            ],
+            metrics: [
+              {
+                name: 'screenPageViews'
+              }
+            ],
+            orderBys: [
+              {
+                metric: {
+                  metricName: 'screenPageViews'
+                },
+                desc: true
+              }
+            ]
+          }
+        },
+        trending: () => {
+          return {
+            property: 'properties/325424669',
+            dateRanges: [
+              {
+                startDate: '1daysAgo',
+                endDate: 'today'
+              }
+            ],
+            dimensions: [
+              {
+                name: 'pagePath'
+              },
+              {
+                name: 'pageTitle'
+              }
+            ],
+            metrics: [
+              {
+                name: 'screenPageViews'
+              }
+            ],
+            orderBys: [
+              {
+                metric: {
+                  metricName: 'screenPageViews'
+                },
+                desc: true
+              }
+            ]
+          }
+        }
+      }
 
-			const summary = {}
+      const summary: {
+        popular?: object[]
+        trending?: object[]
+        imageCount?: number
+      } = {}
 
-			for (const [alias, report] of Object.entries(reports)) {
-				const reportQuery = report()
-				const response = await analyticsData.runReport(reportQuery)
-					.then((value) => {
-						return value[0]
-					})
-					.catch((err) => {
-						console.error(err)
-					})
+      for (const [alias, report] of Object.entries(reports)) {
+        const reportQuery = report()
+        const response = await analyticsData.runReport(reportQuery)
+          .then((value) => {
+            return value[0]
+          })
+          .catch((err) => {
+            console.error(err)
+          })
 
-				let results = {}
+        let results = {}
 
-				if (response && response.rows) {
-					for (const row of response.rows) {
-						let pagePath = ''
-						let removals = []
+        if (response && response.rows) {
+          for (const row of response.rows) {
+            let pagePath = ''
+            const removals = []
 
-						if (row && row.dimensionValues && row.dimensionValues.length > 0 && row.metricValues) {
-							const item = {}
-							for (let idx = 0; idx < row.dimensionValues.length; idx++) {
-								const dimensionKey = <string>reportQuery.dimensions[idx].name
-								let dimensionValue = <string>row.dimensionValues[idx].value
+            if (row && row.dimensionValues && row.dimensionValues.length > 0 && row.metricValues) {
+              const item = {}
+              for (let idx = 0; idx < row.dimensionValues.length; idx++) {
+                const dimensionKey = <string>reportQuery.dimensions[idx].name
+                let dimensionValue = <string>row.dimensionValues[idx].value
 
-								for (const toReplace of removals) {
-									dimensionValue = dimensionValue.replace(toReplace, '')
-								}
+                for (const toReplace of removals) {
+                  dimensionValue = dimensionValue.replace(toReplace, '')
+                }
 
-								if (dimensionKey === 'pagePath') {
-									if (dimensionValue !== '/') {
-										dimensionValue = dimensionValue.replace(/\/$/, '')
-									}
-									pagePath = dimensionValue
-								} else {
-									item[dimensionKey] = dimensionValue
-								}
-							}
+                if (dimensionKey === 'pagePath') {
+                  if (dimensionValue !== '/') {
+                    dimensionValue = dimensionValue.replace(/\/$/, '')
+                  }
+                  pagePath = dimensionValue
+                } else {
+                  item[dimensionKey] = dimensionValue
+                }
+              }
 
-							for (let idx = 0; idx < row.metricValues.length; idx++) {
-								const metricKey = <string>reportQuery.metrics[idx].name
-								const metricValue = <string>row.metricValues[idx].value
-								item[metricKey] = metricValue
-							}
+              for (let idx = 0; idx < row.metricValues.length; idx++) {
+                const metricKey = <string>reportQuery.metrics[idx].name
+                item[metricKey] = <string>row.metricValues[idx].value
+              }
 
-							results[pagePath] = item
-						}
-					}
+              results[pagePath] = item
+            }
+          }
 
-					if (alias === 'basic') {
-						results = {
-							pageViews: response.rows[0].metricValues[0].value,
-							totalVisitors: response.rows[0].metricValues[1].value,
-							newVisitors: response.rows[0].metricValues[2].value,
-							engagementRate: response.rows[0].metricValues[3].value
-						}
-					}
-				}
+          if (alias === 'basic') {
+            results = {
+              pageViews: response.rows[0]?.metricValues[0].value,
+              totalVisitors: response.rows[0]?.metricValues[1].value,
+              newVisitors: response.rows[0]?.metricValues[2].value,
+              engagementRate: response.rows[0]?.metricValues[3].value
+            }
+          }
+        }
 
-				summary[alias] = results
-			}
+        summary[alias] = results
+      }
 
-			summary.popular = toArray(summary.popular)
-			summary.trending = toArray(summary.trending)
+      summary.popular = toArray(summary.popular)
+      summary.trending = toArray(summary.trending)
 
-			return summary
-		})
+      const imageCount = await app.database.getFileCount()
+      summary.imageCount = imageCount
 
-		done()
-	}
+      return summary
+    })
+
+    done()
+  }
 }
