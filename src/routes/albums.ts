a83f1dfecac66f937399805481fdde2870e31482
Dan <danker.twist@gmail.com>
2023-05-07 16:44:24 -0700

    feat: new getAlbum preHandler + add route schemas

--- src/routes/albums.ts
+++ src/routes/albums.ts
@@ -11,9 +11,31 @@ export default class Albums extends Route {
   }
 
   routes(app, options, done) {
+    const getAlbum = async (req, reply) => {
+      if (!req.params.id) return reply.code(404).send({ error: { status: 404, message: 'Album not found' } })
+
+      const album = await app.database.getAlbumById(req.params.id)
+
+      if (!album) return reply.code(404).send({ error: { status: 404, message: 'Album not found' } })
+
+      return req.album = album
+    }
+
     app.get('/', {
       config: {
         auth: false
+      },
+      schema: {
+        querystring: {
+          type: 'object',
+          properties: {
+            search: { type: 'string' },
+            sort: { type: 'string' },
+            order: { type: 'string' },
+            page: { type: 'number' },
+            limit: { type: 'number' }
+          }
+        }
       }
     }, async (req) => {
       const page = req.query.page ? parseInt(req.query.page) : 1
@@ -45,35 +67,48 @@ export default class Albums extends Route {
     app.get('/:id', {
       config: {
         auth: false
-      }
-    }, async (req, reply) => {
-      if (!req.params.id) return reply.code(404).send({ error: { status: 404, message: 'Album not found' } })
+      },
+      schema: {
+        params: {
+          type: 'object',
+          properties: {
+            id: { type: 'string' }
+          }
+        }
+      },
+      preHandler: [getAlbum]
+    }, async (req) => {
+      req.album.fileCount = (await app.database.getAlbumFileCount(req.album.id))?.count ?? 0
 
-      const album = await app.database.getAlbumById(req.params.id)
-
-      if (!album) return reply.code(404).send({ error: { status: 404, message: 'Album not found' } })
-
-      album.fileCount = (await app.database.getAlbumFileCount(album.id))?.count ?? 0
-
-      return album
+      return req.album
     })
 
     app.get('/:id/images', {
       config: {
         auth: false
-      }
-    }, async (req, reply) => {
-      if (!req.params.id) return reply.code(404).send({ error: { status: 404, message: 'Album not found' } })
-
-      const album = await app.database.getAlbumById(req.params.id)
-
-      if (!album) return reply.code(404).send({ error: { status: 404, message: 'Album not found' } })
-
+      },
+      schema: {
+        params: {
+          type: 'object',
+          properties: {
+            id: { type: 'string' }
+          }
+        },
+        querystring: {
+          type: 'object',
+          properties: {
+            page: { type: 'number' },
+            limit: { type: 'number' }
+          }
+        }
+      },
+      preHandler: [getAlbum]
+    }, async (req) => {
       const page = req.query.page ? parseInt(req.query.page) : 1
       const limit = req.query.limit ? parseInt(req.query.limit) : 25
       const pages = (imageCount) => Math.ceil(imageCount / limit)
 
-      let images = await app.database.getAlbumFiles(album.id)
+      let images = await app.database.getAlbumFiles(req.album.id)
       const count = images.length
 
       if (limit !== -1) { // -1 means no limit
