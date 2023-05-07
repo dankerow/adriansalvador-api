6f8647b04be8204b1eed294659defb145afa4485
Dan <danker.twist@gmail.com>
2023-05-07 16:47:02 -0700

    feat: new random route + add route schemas

--- src/routes/images.ts
+++ src/routes/images.ts
@@ -11,16 +11,40 @@ export default class Images extends Route {
   }
 
   routes(app, options, done) {
-    app.get('/:id', async (req, res) => {
+    app.get('/:id', {
+      schema: {
+        params: {
+          type: 'object',
+          properties: {
+            id: { type: 'string' }
+          }
+        },
+        querystring: {
+          type: 'object',
+          properties: {
+            includeAlbum: { type: 'boolean' }
+          }
+        }
+      }
+    }, async (req, reply) => {
       const includeAlbum = req.query.includeAlbum ?? false
       const file = await app.database.getFileById(req.params.id, includeAlbum)
 
-      if (!file) return res.code(404).send({ error: { status: 404, message: 'File not found.' } })
+      if (!file) return reply.code(404).send({ error: { status: 404, message: 'File not found.' } })
 
       return {
         ...file,
         size: filesize(file.size)
       }
+    })
+
+    app.get('/random', {
+      config: {
+        auth: false,
+        rateLimit: { max: 15, timeWindow: 15 * 1000 }
+      }
+    }, async () => {
+      return await app.database.getRandomImages(35)
     })
 
     done()
