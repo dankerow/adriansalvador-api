8356247ff73d3eab1dc545b664511cbdb90e2ac9
Dan <danker.twist@gmail.com>
2023-10-31 18:52:48 -0400

    feat(albums): add additional query options
    This includes the addition of status, favorites and featured query options.
    The favorites route is no longer needed, since the favorite and featured albums can now be fetched from the main route.

--- src/routes/albums.ts
+++ src/routes/albums.ts
@@ -10,7 +10,7 @@ export default class Albums extends Route {
     })
   }
 
-  routes(app, options, done) {
+  routes(app, _options, done) {
     const getAlbum = async (req, reply) => {
       if (!req.params.id) return reply.code(404).send({ error: { status: 404, message: 'Album not found' } })
 
@@ -29,9 +29,12 @@ export default class Albums extends Route {
         querystring: {
           type: 'object',
           properties: {
+            status: { type: 'string', enum: ['all', 'posted', 'draft'] },
+            favorites: { type: 'boolean' },
+            featured: { type: 'boolean' },
             search: { type: 'string' },
             sort: { type: 'string' },
-            order: { type: 'string' },
+            order: { type: 'string', enum: ['asc', 'desc'] },
             page: { type: 'number' },
             limit: { type: 'number' }
           }
@@ -42,8 +45,11 @@ export default class Albums extends Route {
       const limit = req.query.limit ? parseInt(req.query.limit) : 25
 
       const params = {
+        status: req.query.status ?? 'posted',
+        favorites: req.query.favorites ?? null,
+        featured: req.query.featured ?? null,
         search: req.query.search ?? null,
-        sort: req.query.sort ?? { lowerName: 1 },
+        sort: req.query.sort ?? 'lowerName',
         order: req.query.order ?? 'asc',
         limit,
         skip: (page - 1) * limit
@@ -51,7 +57,7 @@ export default class Albums extends Route {
 
       const albums = await app.database.getAlbums(params)
       const count = await app.database.getAlbumCount()
-      const pages = (albumCount) => Math.ceil(albumCount / limit)
+      const pages = (albumCount: number) => Math.ceil(albumCount / limit)
 
       for (const album of albums) {
         album.fileCount = (await app.database.getAlbumFileCount(album.id))?.count ?? 0
@@ -95,7 +101,7 @@ export default class Albums extends Route {
       return await app.database.updateAlbum(req.album.id, { draft: true, postedAt: null })
     })
 
-    app.get('/:id/images', {
+    app.get('/:id/files', {
       config: {
         auth: false
       },
@@ -120,38 +126,21 @@ export default class Albums extends Route {
       const limit = req.query.limit ? parseInt(req.query.limit) : 25
       const pages = (imageCount) => Math.ceil(imageCount / limit)
 
-      let images = await app.database.getAlbumFiles(req.album.id)
-      const count = images.length
+      let files = await app.database.getAlbumFiles(req.album.id)
+      const count = files.length
 
       if (limit !== -1) { // -1 means no limit
-        images = images.slice((page - 1) * limit, page * limit)
+        files = files.slice((page - 1) * limit, page * limit)
       }
 
-      for (const image of images) {
-        image.size = filesize(image.size)
+      for (const file of files) {
+        file.size = filesize(file.size)
       }
 
       return {
-        data: images,
+        data: files,
         count: count,
         pages: pages(count)
-      }
-    })
-
-    app.get('/favorites', {
-      config: {
-        auth: false
-      }
-    }, async () => {
-      const favoriteAlbums = await app.database.getAlbums({ favorite: true })
-      const featuredAlbum = await app.database.getAlbums({ featured: true })
-
-      const albums = [...favoriteAlbums, ...featuredAlbum]
-      const count = albums.length
-
-      return {
-        data: albums,
-        count: count
       }
     })
 
