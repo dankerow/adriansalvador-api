0b0ba4db8c900b378381681474cbe66e405b1d8c
Dan <danker.twist@gmail.com>
2023-11-01 19:11:47 -0400

    chore: separate User interface in UserMetadata and UserCredentials

--- types/user.ts
+++ types/user.ts
@@ -1,9 +1,17 @@
-export interface User {
+export interface UserMetadata {
   readonly id: string
   firstName: string
   lastName: string
-  email: string
-  role: string
   readonly createdAt: string
   readonly updatedAt: string
 }
+
+export interface UserCredentials {
+  readonly id: string
+  email: string
+  password: string
+  readonly createdAt: string
+  readonly updatedAt: string
+}
+
+export type User = UserMetadata & UserCredentials
