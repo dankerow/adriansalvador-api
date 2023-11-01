baccf6a05dd79573eb81dd98ce7af77483f2f233
Dan <danker.twist@gmail.com>
2023-11-01 19:13:33 -0400

    fix: improve types within the auth middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,21 +1,38 @@
+import type { User } from '../../types'
+import type { FastifyReply, FastifyRequest, DoneFuncWithErrOrRes } from 'fastify'
+
 import jwt from 'jsonwebtoken'
 
-export default function (req, res, next) {
-  if (!req.headers.authorization) return res.status(401).send({ message: 'No authorization header provided' })
+interface IBody {
+  user: Omit<User, 'password'>
+}
+
+export default function (req: FastifyRequest<{ Body: IBody }>, reply: FastifyReply, done: DoneFuncWithErrOrRes) {
+  if (!req.headers.authorization) {
+    return reply.status(401).send({ message: 'No authorization header provided' })
+  }
 
   const token = req.headers.authorization.split(' ')[1]
-  if (!token) return res.status(401).send({ message: 'No authorization header provided' })
+  if (!token) {
+    return reply.status(401).send({ message: 'No authorization header provided' })
+  }
 
   jwt.verify(token, process.env.AUTH_SECRET, async (error, decoded) => {
-    if (error) return res.status(401).send({ message: 'Invalid token' })
+    if (error) {
+      return reply.status(401).send({ message: 'Invalid token' })
+    }
 
     const id = decoded?.sub ?? null
-    if (!id) return res.status(401).send({ message: 'Invalid authorization' })
+    if (!id) {
+      return reply.status(401).send({ message: 'Invalid authorization' })
+    }
 
     const user = await this.database.getUserById(id)
-    if (!user) return res.status(401).send({ message: 'User doesn\'t exist' })
+    if (!user) {
+      return reply.status(401).send({ message: 'User doesn\'t exist' })
+    }
 
-    req.user = user
-    next()
+    req.body.user = user
+    done()
   })
 }
